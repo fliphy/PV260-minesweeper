20c1b81d94e9950f8df4d63f38e7351c4c6f209a
Filip <506454@mail.muni.cz>
2021-03-28 14:14:27 +0200

    Project creating

---
+++ MinesWeeper.Tests/MinesWeeperTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using FakeItEasy;
+
+namespace MinesWeeper.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ MinesWeeper/Item.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MinesWeeper
+{
+    public class Item
+    {
+    }
+}
