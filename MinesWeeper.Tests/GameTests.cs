8f91790a23512c4f6449ccf970c5c2a06bbed5b3
Ivan Kiral <kiralivan@gmail.com>
2021-03-31 16:32:02 +0200

    Test game throws exception

--- MinesWeeper.Tests/GameTests.cs
+++ MinesWeeper.Tests/GameTests.cs
@@ -1,11 +1,26 @@
+using System;
 using System.Collections.Generic;
+using System.Net.Mime;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 
 namespace MinesWeeper.Tests
 {
     public class GameTests
     {
+
+        [Test]
+        [TestCase(2,3)]
+        [TestCase(3,2)]
+        [TestCase(51, 50)]
+        [TestCase(50, 51)]
+        public void Game_StartGame_ThrowArgumentException(int width, int height)
+        {
+            var board = new Board();
+            var game = new Game(board);
+            Assert.Throws<ArgumentException>(() => game.StartGame(width, height));
+        }
         
         [Test]
         [TestCase(10, 10)]
